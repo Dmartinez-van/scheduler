f5677b4488e99be0068d84aa29cb919f8b5ce0c5
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-05-12 12:36:13 -0700

    finish w7d1 work

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,34 +2,86 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from "components/Appointment";
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 0,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 1,
+  },
+  {
+    id: 4,
+    name: "Thursday",
+    spots: 1,
+  },
+];
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "David Martinez",
+      interviewer: {
+        id: 3,
+        name: "Tommy Phillpo",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Garbango Rocket",
+      interviewer: {
+        id: 5,
+        name: "Kaleb Longbow",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
 
 export default function Application(props) {
-  
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 0,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 1,
-    },
-    {
-      id: 4,
-      name: "Thursday",
-      spots: 1,
-    },
-  ];
 
   const [day, setDay] = useState("Monday");
   console.log(day);
+
+  const appointmentArr = appointments.map(appointment => {
+    return <Appointment key={appointment.id} {...appointment} />
+  })
 
   return (
     <main className="layout">
@@ -46,7 +98,8 @@ export default function Application(props) {
           <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs"/>
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements during the "The Scheduler" activity. */}
+        {appointmentArr}
+        {<Appointment key="last" time="5pm"/>}
       </section>
     </main>
   );
