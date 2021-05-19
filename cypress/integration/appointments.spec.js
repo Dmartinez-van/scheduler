2218bc8a209670e137519512722f6362a7aa2ae2
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-05-19 13:46:10 -0700

    Add template for appointment tests

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,17 @@
+describe("Tests for functionality of Appointments", () => {
+  it("should book an interview", () => {
+    cy.visit("/")
+    cy.contains("Monday")
+  });
+
+
+  // it("should book an interview", () => {
+
+  // });
+
+
+  // it("should book an interview", () => {
+
+  // });
+
+});
